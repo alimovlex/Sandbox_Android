2e768d1f8f5edd2649cb5aac4f90bb440939f1d5
alimovlex <alimovlex@hotmail.com>
2023-01-22 05:30:12 +0100

    Assembled the C++ NDK android project without Android Studio and gradle..

---
+++ android-ndk-without-gradle/README.md
@@ -0,0 +1,63 @@
+# android-ndk-without-gradle
+
+This is an example of building and packaging an Android NDK application without using Gradle - it relies on CMake only.
+
+Again, this is an example. It's not a tutorial and you should think twice before using this in any form of production.
+
+## TL;DR
+
+If you want to try this project:
+
+1. Run something like this: `sh build.sh`
+1. ???
+1. Profit
+
+This assumes you have the Android NDK installed and CMake. It will use the latest version available for each required component.
+
+## With a little bit more details:
+
+After installing the Android SDK and its NDK, it will be in a folder like this: `<path-to-sdk>/ndk/<ndk-version>`.
+
+The CMake project is a little bit metamorphic. When run without any parameters, it behaves like the type of CMake file you would normally use with Gradle - i.e. it generates a shared library.
+
+The real trick is the secret sauce added when you run it with the special flag (`-DUMBRELLA=1`). That flag will take over the build.
+
+First, it adds the project itself as an ExternalProject without the special flag. This allows CMake to run the build just like Gradle would do. It does it four times, one per ABI.
+
+Then it is a matter of calling Android tools to package and sign the APK.
+
+There are extra tricks to make this work: the shared library definition also have a `install` directive. This seems to be required by CMake. And the ExternalProject invocation puts the output of any library in the exact place Android will expect it in the APK.
+
+## Q&A
+
+Some questions you may have if you want to use this project:
+
+* **Why CMake?**
+
+  Because I like it and I use it for everything C++ related.
+* **Makefiles or Ninja?**
+
+  It doesn't matter. As a personal preference, use Ninja. When automatically generated, Ninja is superior to Makefiles - specially because it's clever enough to run parallel builds without flags.
+
+## What's wrong with Gradle?
+
+This deserves its own section:
+
+* **Android builds with Gradle requires a server running**
+
+  You are probably doing some architectural mistakes if you need a cache server to do normal builds. It is useful if you do a daily clean build of a legacy large-scale project, but it is a bad sign when you need it when coding.
+* **Gradle forces you to learn Groovy**
+
+  This [XKCD strip](https://xkcd.com/927/) describes why another build standard doesn't work:
+  ![XKCD take on standards](https://imgs.xkcd.com/comics/standards.png) 
+* **Gradle forces you to use specific versions of CMake**
+
+  This is annoying when you want to use the latest and greatest of CMake.
+* **It is slow**
+
+  The first build can take up to a minute to complete, between server starting and other Gradle shenanigans. 
+
+  An incremental build takes precious seconds until it invokes CMake. That adds up and it is frustrating.
+
+  Even a no-op (builds with no change) takes its time. Two seconds if the server is running, eight seconds otherwise. This is insane, compared to Ninja's 17ms.,
+
---
+++ android-ndk-without-gradle/apk_build.cmake
@@ -0,0 +1,88 @@
+include(ExternalProject)
+
+set(ANDROID_SDK_ROOT $ENV{ANDROID_SDK_ROOT} CACHE PATH "/home/robot/android")
+
+set(ANDROID_PLATFORM_TOOLS_DIR "${ANDROID_SDK_ROOT}/platform-tools")
+
+file(GLOB ANDROID_BUILD_TOOLS_VERSIONS "${ANDROID_SDK_ROOT}/build-tools/*")
+list(POP_BACK ANDROID_BUILD_TOOLS_VERSIONS ANDROID_BUILD_TOOLS_LATEST)
+set(ANDROID_BUILD_TOOLS_DIR "${ANDROID_BUILD_TOOLS_LATEST}" CACHE PATH "/home/robot/android/build-tools/29.0.3")
+
+file(GLOB ANDROID_NDK_VERSIONS "${ANDROID_SDK_ROOT}/ndk-bundle/*")
+list(POP_BACK ANDROID_NDK_VERSIONS ANDROID_NDK_LATEST)
+set(ANDROID_NDK "${ANDROID_NDK_LATEST}" CACHE PATH "/home/robot/android/ndk-bundle")
+
+file(GLOB ANDROID_JAR_VERSIONS "${ANDROID_SDK_ROOT}/platforms/*")
+list(POP_BACK ANDROID_JAR_VERSIONS ANDROID_JAR_LATEST)
+set(ANDROID_JAR "${ANDROID_JAR_LATEST}/android.jar" CACHE PATH "/home/robot/android/platforms/android-29")
+
+find_program(ADB       NAMES adb       REQUIRED PATHS ${ANDROID_PLATFORM_TOOLS_DIR})
+find_program(AAPT2     NAMES aapt2     REQUIRED PATHS ${ANDROID_BUILD_TOOLS_DIR})
+find_program(APKSIGNER NAMES apksigner REQUIRED PATHS ${ANDROID_BUILD_TOOLS_DIR})
+find_program(ZIPALIGN  NAMES zipalign  REQUIRED PATHS ${ANDROID_BUILD_TOOLS_DIR})
+
+set(APK_CONTENTS_ROOT "${CMAKE_CURRENT_BINARY_DIR}/apk")
+
+macro(setup_variant VARIANT)
+  ExternalProject_Add(${PROJECT_NAME}-${VARIANT}
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+    EXCLUDE_FROM_ALL TRUE
+    CMAKE_ARGS
+    -DANDROID_ABI=${VARIANT}
+    -DANDROID_NDK=/home/robot/android/ndk-bundle
+    -DANDROID_STL=c++_static
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/install/${VARIANT}
+    -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${APK_CONTENTS_ROOT}/lib/${VARIANT}
+    -DCMAKE_TOOLCHAIN_FILE=/home/robot/android/ndk-bundle/build/cmake/android.toolchain.cmake
+    )
+endmacro()
+setup_variant(arm64-v8a)
+setup_variant(armeabi-v7a)
+setup_variant(x86)
+setup_variant(x86_64)
+
+set(MANIFEST "${CMAKE_CURRENT_SOURCE_DIR}/AndroidManifest.xml")
+set(RESOURCES "${CMAKE_CURRENT_SOURCE_DIR}/res")
+set(KEYSTORE "${CMAKE_CURRENT_SOURCE_DIR}/keystore.jks")
+
+set(VALUES_STRING "${CMAKE_CURRENT_BINARY_DIR}/values_strings.arsc.flat")
+
+set(FINAL_APK "${CMAKE_CURRENT_BINARY_DIR}/app.apk")
+set(RESOURCES_APK "${CMAKE_CURRENT_BINARY_DIR}/app.res.apk")
+set(UNALIGNED_APK "${CMAKE_CURRENT_BINARY_DIR}/app.unaligned.apk")
+
+#keytool -genkeypair -keystore keystore.jks -alias androidkey -validity 10000 -keyalg RSA -keysize 2048 -storepass android -keypass android
+
+add_custom_command(
+  OUTPUT ${VALUES_STRING}
+  COMMAND ${AAPT2} compile ${RESOURCES}/values/strings.xml -o ${CMAKE_CURRENT_BINARY_DIR}
+  DEPENDS ${RESOURCES}/values/strings.xml
+  )
+add_custom_command(
+  OUTPUT ${RESOURCES_APK}
+  COMMAND ${AAPT2} link ${VALUES_STRING} -o ${RESOURCES_APK} --manifest ${MANIFEST} -I /home/robot/android/platforms/android-29/android.jar
+  DEPENDS ${MANIFEST} ${VALUES_STRING}
+  )
+add_custom_command(
+  OUTPUT ${UNALIGNED_APK}
+  COMMAND ${CMAKE_COMMAND} -E tar x ${RESOURCES_APK}
+  COMMAND ${CMAKE_COMMAND} -E tar c ${UNALIGNED_APK} --format=zip .
+  WORKING_DIRECTORY ${APK_CONTENTS_ROOT}
+  DEPENDS
+    ${RESOURCES_APK}
+    ${PROJECT_NAME}-armeabi-v7a
+    ${PROJECT_NAME}-arm64-v8a
+    ${PROJECT_NAME}-x86
+    ${PROJECT_NAME}-x86_64
+  )
+add_custom_command(
+  OUTPUT ${FINAL_APK}
+  COMMAND ${ZIPALIGN} -p -f -v 4 ${UNALIGNED_APK} ${FINAL_APK}
+  COMMAND keytool -genkey -v -keystore my-release-key.keystore -alias standkey -keyalg RSA -keysize 2048 -validity 10000 -storepass password -keypass password -dname "CN=example.com, OU=ID, O=Example, L=Doe, S=John, C=GB"
+  COMMAND ${APKSIGNER} sign --in ${FINAL_APK} -ks my-release-key.keystore --ks-key-alias standkey --ks-pass pass:password --key-pass pass:password
+  DEPENDS ${UNALIGNED_APK}
+  )
+
+add_custom_target(apk ALL DEPENDS ${FINAL_APK})
+
+install(CODE "execute_process(COMMAND ${ADB} install ${FINAL_APK})")
