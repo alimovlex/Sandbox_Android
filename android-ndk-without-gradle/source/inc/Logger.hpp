ac90dd42c95fb8b1142d868513e22a884b36e0f9
alimovlex <alimovlex@hotmail.com>
2024-04-13 00:26:52 +0200

    Added new project to the Sandbox

--- android-ndk-without-gradle/source/inc/Logger.hpp
+++
@@ -1,7 +0,0 @@
-//#include <android_native_app_glue.h>
-#include <android/log.h>
-#include <android/native_activity.h>
-#include <android/asset_manager.h>
-#define APPNAME "hello-world"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, APPNAME, __VA_ARGS__)
-#define LOGA(cond, ...) __android_log_assert(cond, APPNAME, __VA_ARGS__)
--- android-ndk-without-gradle/source/main.cpp
+++ android-ndk-without-gradle/source/main.cpp
@@ -2,7 +2,7 @@
 #include "Tutorial.hpp"
 
 JNIEXPORT
-void ANativeActivity_onCreate(ANativeActivity * /*activity*/, void * /*savedState*/, size_t /*savedStateSize*/) 
+void ANativeActivity_onCreate(ANativeActivity * /*activity*/, void * /*savedState*/, size_t /*savedStateSize*/)
 {
   //__android_log_print(ANDROID_LOG_INFO, "hello-world", "Well, this worked!"); // NOLINT
   //timerFunction(fileTest);
